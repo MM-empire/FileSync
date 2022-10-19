3d2cd0da0bfca179691c84f522623dc2dbfa1f12
Iwwww <mike.yolgin@gmail.com>
2022-10-19 15:20:52 +0300

    Refactor to libpath.Path in json_handler.py

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,49 +1,51 @@
+#!/usr/bin/env python3
 from json import dump, load
-from os.path import exists
+#  from os.path import exists
+from pathlib import Path
 from typing import Dict, Any
 
 
 class JsonHandler():
-    def __init__(self, path: str) -> None:
-        self.__path: str = path
-        if not exists(path):
+    def __init__(self, path: Path) -> None:
+        self.__path: Path = path
+        if not path.exists():
             self.write({})
             
     @property
-    def path(self) -> str:
+    def path(self) -> Path:
         return self.__path
 
     @path.setter
-    def path(self, path: str) -> None:
+    def path(self, path: Path) -> None:
         self.__path = path
     
-    def read(self) -> Dict[str, Any]:
+    def read(self) -> Dict[Path, Any]:
         with open(self.path, 'r') as file:
-            data: Dict[str, Any] = load(file)
+            data: Dict[Path, Any] = load(file)
         return data
 
-    def write(self, data: Dict[str, Any]) -> None:
+    def write(self, data: Dict[Path, Any]) -> None:
         with open(self.path, 'w') as file:
             dump(data, file, indent=4)
 
-    def add_origin(self, origin_path: str) -> None:
-        data: Dict[str, Any] = self.read()
+    def add_origin(self, origin_path: Path) -> None:
+        data: Dict[Path, Any] = self.read()
         data.update({origin_path: {'metadata': {'hash': None, 'changed': True},
             'copies': {}}})
         self.write(data)
         
-    def add_copy(self, origin_path: str, copy_path: str) -> None:
-        data: Dict[str, Any] = self.read()
-        copy_dict: Dict[str, Dict[str, None]] = {copy_path: {'hash': None}}
+    def add_copy(self, origin_path: Path, copy_path: Path) -> None:
+        data: Dict[Path, Any] = self.read()
+        copy_dict: Dict[Path, Dict[str, None]] = {copy_path: {'hash': None}}
         data[origin_path]['copies'].update(copy_dict)
         self.write(data);
 
 
 def main() -> None:
-    jh = JsonHandler('store.json')
-    jh.add_origin('file1.txt')
-    jh.add_origin('file2.txt')
-    jh.add_copy('file1.txt', 'file3.txt')
+    jh = JsonHandler(Path('store.json'))
+    jh.add_origin(Path('file1.txt'))
+    jh.add_origin(Path('file2.txt'))
+    jh.add_copy(Path('file1.txt'), Path('file3.txt'))
     print(jh.read())
 
 
