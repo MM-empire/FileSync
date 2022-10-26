bb88a041f5c9ebab0b579a36170829fa7429805c
Iwwww <45224503+Iwwww@users.noreply.github.com>
2022-10-26 14:50:48 +0300

    Revert "Matvei"

--- src/hash_handler.py
+++ src/hash_handler.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python3
 
-from hashlib import new
-from pathlib import Path
+from hashlib import sha1
+from os.path import exists
 
 
 class HashHandler():
-    @staticmethod
-    def __byte_read_file(path: Path) -> bytes:
-        with open(str(path), 'rb') as file:
+    def __init__(self, path: str) -> None:
+        self.__path: str = path
+        if exists(path):
+            self.__hash_hex: str = self.__calculateHash(self.__path)
+        else:
+            pass
 
+    @property
+    def hash(self) -> str:
+        return self.__hash_hex
+
+    @property
+    def path(self) -> str:
+        return self.__path
+
+    @path.setter
+    def path(self, path: str) -> None:
+        self.__path = path
+        if exists(path):
+            self.__hash_hex = self.__calculateHash(self.__path)
+        else:
+            pass
+
+    def compareHash(self, comp_file_path: str) -> bool:
+        if (self.__hash_hex == self.__calculateHash(comp_file_path)):
+            return True
+
+        return False
+
+    def __byteReadFile(self, path: str) -> bytes:
+        with open(path, 'rb') as file:
             return file.read()
 
-    @staticmethod
-    def calculate_hash(path: Path, algorithm: str='sha1') -> str:
-        buf: bytes = HashHandler.__byte_readFile(path)
-        hasher = new(algorithm)
+    def __calculateHash(self, path: str = '') -> str:
+        if (path == ''):
+            path = self.__path
+
+        buf: bytes = self.__byteReadFile(path)
+        hasher = sha1()
         hasher.update(buf)
         hash_hex = hasher.hexdigest()
         return hash_hex
@@ -22,7 +51,12 @@ class HashHandler():
 
 def main() -> None:
     # file1.file does not exists
-    print(HashHandler.calculate_hash(Path("file.txt")))
+    h = HashHandler("file1.file")
+    h = HashHandler("file2.file")
+    h.path = "file.file"
+    print(h.compareHash("file2.file"))
+    print(h.hash)
+    print(h.path)
 
     """
     TODO:
--- src/json_handler.py
+++ src/json_handler.py
@@ -1,102 +1,49 @@
 from json import dump, load
-from pathlib import Path
-from typing import Dict, Any, List, Optional
-
-from exceptions import CopyDoesNotExistsError, \
-        OriginDoesNotExistsError
+from os.path import exists
+from typing import Dict, Any
 
 
 class JsonHandler():
-    def __init__(self, path: Path) -> None:
-        self.path: Path = path
-        if not path.exists():
+    def __init__(self, path: str) -> None:
+        self.__path: str = path
+        if not exists(path):
             self.write({})
             
     @property
-    def path(self) -> Path:
+    def path(self) -> str:
         return self.__path
 
     @path.setter
-    def path(self, path: Path) -> None:
-        if not str(path).endswith('.json'):
-            raise FileNotFoundError
+    def path(self, path: str) -> None:
         self.__path = path
     
     def read(self) -> Dict[str, Any]:
-        with open(str(self.path), 'r') as file:
-            return load(file)
+        with open(self.path, 'r') as file:
+            data: Dict[str, Any] = load(file)
+        return data
 
     def write(self, data: Dict[str, Any]) -> None:
-        with open(str(self.path), 'w') as file:
+        with open(self.path, 'w') as file:
             dump(data, file, indent=4)
 
-    def exists_origin(self, origin: Path) -> bool:
+    def add_origin(self, origin_path: str) -> None:
         data: Dict[str, Any] = self.read()
-        if data.get(str(origin)):
-            return True
-        return False
-
-    def exists_copy(self, origin: Path, copy: Path) -> bool:
-        data: Dict[str, Any] = self.read()
-        if data[str(origin)]['copies']:
-            return True
-        return False
-
-    def check_existing(self, origin: Path, copy: Optional[Path]=None):
-        if not self.exists_origin(origin):
-            raise OriginDoesNotExistsError(origin)
-        if copy and not self.exists_copy(origin, copy):
-            print(str(copy))
-            raise CopyDoesNotExistsError(origin, copy)
-
-    def add_origin(self, origin: Path) -> None:
-        if not self.exists_origin(origin):
-            origin_dict: Dict[str, Any] = {str(origin): {'metadata': \
-                    {'hash': None, 'changed': True}, 'copies': {}}}
-            data: Dict[str, Any] = self.read()
-            data.update(origin_dict)
-            self.write(data)
+        data.update({origin_path: {'metadata': {'hash': None, 'changed': True},
+            'copies': {}}})
+        self.write(data)
         
-    def add_copy(self, origin: Path, copy: Path) -> None:
-        self.check_existing(origin)
-        if not self.exists_copy(origin, copy):
-            data: Dict[str, Any] = self.read()
-            copy_dict: Dict[str, Dict[str, None]] = {str(copy): {'hash': None}}
-            data[str(origin)]['copies'].update(copy_dict)
-            self.write(data);
-
-    def get_origins(self) -> List[Path]:
+    def add_copy(self, origin_path: str, copy_path: str) -> None:
         data: Dict[str, Any] = self.read()
-        return [Path(path) for path in data.keys()]
+        copy_dict: Dict[str, Dict[str, None]] = {copy_path: {'hash': None}}
+        data[origin_path]['copies'].update(copy_dict)
+        self.write(data);
 
-    def get_copies(self, origin: Path) -> List[Path]:
-        data: Dict[str, Any] = self.read()
-        return [Path(path) for path in data[str(origin)]['copies'].keys()]
-
-    def remove_origin(self, origin: Path) -> None:
-        self.check_existing(origin)
-        data: Dict[str, Any] = self.read()
-        data.__delitem__(str(origin))
-        self.write(data)
-
-    def remove_copy(self, origin: Path, copy: Path) -> None:
-        self.check_existing(origin, copy)
-        data: Dict[str, Any] = self.read()
-        data[str(origin)]['copies'].__delitem__(str(copy))
-        self.write(data)
-
-    def get_changed_origins(self) -> List[Path]:
-        data: Dict[str, Any] = self.read()
-        changed_origins: List[Path] = [Path(path) for path in
-                data if data[path]['metadata']['changed'] == True]
-
-        return changed_origins
 
 def main() -> None:
-    jh = JsonHandler(Path('store.json'))
-    jh.add_origin(Path('file1.txt'))
-    jh.add_origin(Path('/home/user/sandbox/python/file.txt'))
-    jh.add_copy(Path('file1.txt'), Path('file3.txt'))
+    jh = JsonHandler('store.json')
+    jh.add_origin('file1.txt')
+    jh.add_origin('file2.txt')
+    jh.add_copy('file1.txt', 'file3.txt')
     print(jh.read())
 
 
